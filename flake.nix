c1fab472725e8b0bf69add8a2e3813d365b2984a
Lucas Savva <lucas@m1cr0man.com>
2022-09-06 23:20:35 +0100

    Another massive design shift
    Going to use qp2p instead.
    Switched to Nix flakes.
    den lib anyone?

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "QuicFS Rust";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        rustVersion = "1.63.0";
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      rec {
+        devShell = devShells.default;
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkgconfig
+            (rust-bin.stable."${rustVersion}".default.override {
+              extensions = [ "rust-src" "rustfmt" ];
+            })
+            lldb
+            rust-analyzer
+            cmake
+            libev
+            uthash
+          ];
+        };
+      }
+    );
+}
--- shell.nix
+++ shell.nix
@@ -1,19 +1,16 @@
+# Legacy shell for use with nix env selector vscode plugin
+# Exposes the devShell attribute from the flake.nix outputs
 let
-  rustVersion = "1.57.0";
-  defaultPkgs = import (builtins.getFlake "nixpkgs") {
-    overlays = [ (builtins.getFlake "github:oxalica/rust-overlay").overlay ];
-  };
-in { pkgs ? defaultPkgs }:
-with pkgs;
-mkShell {
-  buildInputs = [
-    (rust-bin.stable."${rustVersion}".default.override {
-        extensions = [ "rust-src" "rustfmt" ];
-    })
-    lldb
-    rust-analyzer
-    cmake
-    libev
-    uthash
-  ];
-}
+  rev = "12c64ca55c1014cdc1b16ed5a804aa8576601ff2";
+  flake = (import
+    (
+      fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${rev}.tar.gz";
+        sha256 = "0jm6nzb83wa6ai17ly9fzpqc40wg1viib8klq8lby54agpl213w5";
+      }
+    )
+    {
+      src = ./.;
+    });
+in
+flake.shellNix
