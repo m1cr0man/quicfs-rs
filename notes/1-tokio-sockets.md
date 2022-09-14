ce0a0899279eaaf016c5addc6884cf72b2453553
Lucas Savva <lucas@m1cr0man.com>
2022-09-14 02:02:13 +0100

    Add working tokio telnet chat server

---
+++ notes/1-tokio-sockets.md
@@ -0,0 +1,27 @@
+# Tokio Sockets
+
+Following [this video](https://www.youtube.com/watch?v=T2mWg91sx-o).
+
+Got very lost when socket.read_line was giving an unsatisfied trait error.
+Had to switch to using reader.
+https://users.rust-lang.org/t/how-can-i-read-line-by-line-from-tokio-tcpstream/38665
+
+split() is used so that we can delegate ownership of read to
+the BufReader and still use write in the loop.
+
+async move allows us to create a future, avoids simply writing a new function
+Kind of like an async lambda I guess?
+
+"Type inside async block must be known in this context" -> a generic type can't be
+inferred. Use turbo fish ::\<T\> to specify the type
+
+"Use of moved value tx" -> this is why we have to clone in loop
+
+broadcast channel rx was not cloned in the loop, instead we use tx.subscribe().
+Just a design quirk.
+
+
+tokio::select! -> waitForAny/gather/go select
+
+Spawn vs select: Consider the shared state. Select is better when there's
+shared state, spawn is better otherwise.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,63 @@
-fn main() {
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+    sync::broadcast,
+};
+
+#[tokio::main]
+async fn main() {
     let out = "Hello world!";
     println!("{}", out);
+
+    let listener = TcpListener::bind("localhost:8012").await.unwrap();
+
+    let (tx, _rx) = broadcast::channel(10);
+
+    // This loop allows us to accept multiple connections
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        // Quirk: Clone the rx from the tx, rather than the original rx
+        let mut rx = tx.subscribe();
+
+        // Spawn a new thread to handle this connection
+        // async move allows us to create a future, avoids simply writing a new function
+        // Kind of like an async lambda I guess?
+        tokio::spawn(async move {
+            // Can't be in the loop due to the move
+            let (read, mut write) = socket.split();
+
+            // Note: BufReader owns the entire socket if we didn't split.
+            // Hence we only pass the relevant half.
+            let mut reader = BufReader::new(read);
+            let mut line = String::new();
+
+            loop {
+                // Use select so we can read + write at the same time
+                tokio::select! {
+                    // Note: await is implicit
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+
+                        tx.send((line.clone(), addr)).unwrap();
+
+                        // Wipe the line buffer each time
+                        line.clear();
+                    }
+
+                    result = rx.recv() => {
+                        let (msg, recv_addr) = result.unwrap();
+                        // Don't repeat what the current connection sent
+                        if recv_addr != addr {
+                            // line.as_bytes -> provides underlying bytes
+                            write.write_all(&msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
