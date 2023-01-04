6d1f40df020d7dfe4d87c2798af7a066359cf79b
Lucas Savva <lucas@m1cr0man.com>
2023-01-04 22:33:22 +0000

    WIP libp2p

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-fn main() {
-    let out = "Hello world!";
-    println!("{}", out);
+use std::error::Error;
+
+use libp2p::{identity, quic, PeerId};
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+
+    let quic_config = quic::Config::new(&local_key);
+    let mut transport = quic::async_std::Transport::new(quic_config);
+
+    println!("Local peer ID: {:?}", local_peer_id);
+    Ok(())
 }
